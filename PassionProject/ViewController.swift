58cc6f86f9a63e495c3d9925a94d85d826ee1558
Nandan Pericherla <79297356+rockdude129@users.noreply.github.com>
2023-02-25 12:18:21 -0800

    Add files via upload

---
+++ PassionProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PassionProject
+//
+//  Created by Nandan Pericherla on 12/1/22.
+//
+
+import UIKit
+import Foundation
+import SwiftUI
+
+//The Functions
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func aButton(_ sender: Any) {
+        print("A button clicked")
+    }
+}
+
